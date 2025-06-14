63a9a3b584a030725c29058e2077c08c7773311a
MasRozak <mygdrives01@gmail.com>
2025-06-14 17:05:38 +0700

    fix product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { getAllProducts} = require("../controllers/productController");
-const { getBestSellingProducts } = require("../controllers/productController");
-const { getProductById } = require("../controllers/productController");
 
 const {
   getBestSellingProducts,
