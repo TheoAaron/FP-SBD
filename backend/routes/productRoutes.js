785fdc35df824020a2408f30f16e4fdd964fa8f8
yudi0312 <sakuragungde@gmail.com>
2025-06-14 16:17:30 +0700

    add bestselling&productid

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+<<<<<<< HEAD
 const { getAllProducts} = require("../controllers/productController");
 
 router.get("/products", getAllProducts);
 // router.get("/products/:id", getProductById);
+=======
+const {
+  getBestSellingProducts,
+  getProductById,
+} = require("../controllers/productController"); 
+
+router.get("/bs", getBestSellingProducts);    
+router.get("/:id", getProductById);
+
+>>>>>>> f938691 (add bestseller&singleproduct api)
 
 module.exports = router;
