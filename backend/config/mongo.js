2e465ecab5f0009e66d272bf67a98d363f11f268
TheoAaron <theodorusaaron31@gmail.com>
2025-06-18 20:34:49 +0800

    Adding Mongo Database

--- backend/config/mongo.js
+++ backend/config/mongo.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require("mongodb");
+import { MongoClient } from "mongodb";
 
 const MONGO_URI = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.DB_NAME}`;
 const client = new MongoClient(MONGO_URI);
@@ -8,17 +8,21 @@ let dbConnection;
 const connectDB = async () => {
   if (dbConnection) {
     console.log("MongoDB already connected ✅");
-    return dbConnection;
+    return;
   }
   try {
     await client.connect();
     dbConnection = client.db(process.env.DB_NAME);
     console.log("MongoDB connected ✅");
-    return dbConnection;
   } catch (err) {
     console.error("MongoDB connection error ❌", err.message);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+const getDB = () => {
+  if (!dbConnection) throw new Error("DB not connected");
+  return dbConnection;
+};
+
+export { connectDB, getDB };
